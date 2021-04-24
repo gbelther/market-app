6435c557cc808dd84d0e4611e5cfffb3c562f3b1
GBelther <belthergabriel@gmail.com>
2021-04-24 15:59:29 -0300

    Change name Product Page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 import { Cart } from "./pages/Cart";
 import { Home } from "./pages/Home";
-import { Product } from "./pages/Product";
+import { AboutProduct } from "./pages/AboutProduct";
 
 export function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/product" component={Product} />
+      <Route path="/product" component={AboutProduct} />
       <Route path="/cart" component={Cart} />
     </Switch>
   );
---
+++ src/services/api.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://run.mocky.io/v3/c7c87764-f0ae-4723-906d-39d16728525a",
+});
