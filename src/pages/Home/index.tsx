c8aa3c88284fbfb6d39addbeadc5a1adbebfb45a
GBelther <belthergabriel@gmail.com>
2021-04-25 19:49:51 -0300

    Create a filter to products list in Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { FormEvent, useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Product } from "../../components/Product";
 import { api } from "../../services/api";
@@ -22,9 +22,22 @@ interface IProduct {
 }
 
 export function Home() {
+  const [filteredProducts, setFilteredProducts] = useState<IProduct[]>([]);
   const { products } = useContext(ProductsContext);
 
-  console.log(products);
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, []);
+
+  function handleAddFilter(event: FormEvent<HTMLInputElement>) {
+    const filtered = products.filter((product) =>
+      product.description
+        .toLocaleLowerCase()
+        .includes((event.target as HTMLInputElement).value.toLocaleLowerCase())
+    );
+
+    setFilteredProducts(filtered);
+  }
 
   return (
     <>
@@ -33,12 +46,16 @@ export function Home() {
       <div className="container-home">
         <main className="container-main">
           <section className="inputs-wrapper">
-            <input type="text" name="" placeholder="Filtrar produto..." />
+            <input
+              type="text"
+              placeholder="Filtrar produto..."
+              onChange={handleAddFilter}
+            />
           </section>
 
           <section className="products-wrapper">
-            {products ? (
-              products.map((product) => (
+            {filteredProducts ? (
+              filteredProducts.map((product) => (
                 <Product key={product.id} product={product} />
               ))
             ) : (
