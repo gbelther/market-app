18e96512ad515ac420da5b44e7d9e5c3eb908bdc
GBelther <belthergabriel@gmail.com>
2021-04-25 15:52:02 -0300

    Create a hook to modify products in cart - AddProduct, DeleteProduct and Decrease Product

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,113 @@
+import { useContext, useEffect, useState } from "react";
+import { ProductsContext } from "../contexts/ProductsContext";
+
+interface IProduct {
+  id: number;
+  order_number: number;
+  price: number;
+  image: string;
+  description: string;
+  stock: number;
+  offer?: number;
+  promotion?: {
+    kind: string;
+    base: number;
+    value: number;
+  };
+}
+
+interface ICart {
+  id: number;
+  order_number: number;
+  price: number;
+  image: string;
+  description: string;
+  stock: number;
+  offer?: number;
+  promotion?: {
+    kind: string;
+    base: number;
+    value: number;
+  };
+  amount: number;
+}
+
+export function useCart() {
+  const [cart, setCart] = useState<ICart[]>(() => {
+    const cartStorage = localStorage.getItem("MarketApp:cart");
+    console.log(cartStorage);
+
+    if (cartStorage) return JSON.parse(cartStorage);
+
+    return [];
+  });
+
+  useEffect(() => {}, []);
+
+  function productAmount(product: IProduct) {
+    const productInCart = cart.find((prod) => prod.id === product.id);
+
+    return productInCart ? Number(productInCart.amount) : 0;
+  }
+
+  function addProduct(product: IProduct) {
+    const productInCart = cart.find((prod) => prod.id === product.id);
+
+    if (!productInCart) {
+      setCart([...cart, { ...product, amount: 1 }]);
+      localStorage.setItem(
+        "MarketApp:cart",
+        JSON.stringify([...cart, { ...product, amount: 1 }])
+      );
+
+      return;
+    }
+
+    if (productInCart && product.stock > productInCart.amount) {
+      const updatedCart = cart.map((p) =>
+        p.id === product.id ? { ...p, amount: Number(p.amount) + 1 } : p
+      );
+
+      setCart(updatedCart);
+
+      localStorage.setItem("MarketApp:cart", JSON.stringify(updatedCart));
+      return;
+    } else {
+      alert("Produto sem estoque!");
+    }
+  }
+
+  function decrementProduct(product: IProduct) {
+    const productInCart = cart.find((prod) => prod.id === product.id);
+
+    if (productInCart) {
+      if (productInCart.amount > 1) {
+        const updatedCart = cart.map((p) =>
+          p.id === product.id ? { ...p, amount: Number(p.amount) - 1 } : p
+        );
+
+        setCart(updatedCart);
+
+        localStorage.setItem("MarketApp:cart", JSON.stringify(updatedCart));
+        return;
+      } else {
+        deleteProduct(product);
+      }
+    }
+  }
+
+  function deleteProduct(product: IProduct) {
+    const productInCart = cart.find((prod) => prod.id === product.id);
+
+    if (productInCart) {
+      const updatedCart = cart.filter((p) => p.id !== productInCart.id);
+
+      setCart(updatedCart);
+
+      localStorage.setItem("MarketApp:cart", JSON.stringify(updatedCart));
+      return;
+    }
+  }
+
+  return { productAmount, addProduct, decrementProduct };
+}
