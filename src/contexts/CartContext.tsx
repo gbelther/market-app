302c91d194edf474e816ff040d7684ac4184164b
GBelther <belthergabriel@gmail.com>
2021-04-26 13:29:03 -0300

    Change the hook useCart by a context CartContext because I was unable to access the dynamically updated variables

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-interface IProduct {
+interface IProductInCart {
   id: number;
   order_number: number;
   price: number;
@@ -15,7 +16,7 @@ interface IProduct {
   };
 }
 
-interface ICart {
+interface IProduct {
   id: number;
   order_number: number;
   price: number;
@@ -31,8 +32,26 @@ interface ICart {
   amount: number;
 }
 
-export function useCart() {
-  const [cart, setCart] = useState<ICart[]>(() => {
+interface ICartContextData {
+  cart: IProduct[];
+  productAmount: (product: IProductInCart) => number;
+  addProduct: (product: IProductInCart) => void;
+  decrementProduct: (product: IProductInCart) => void;
+  deleteProduct: (product: IProductInCart) => void;
+}
+
+interface ICartDataContextProps {
+  children: ReactNode;
+}
+
+export const CartContext = createContext<ICartContextData>(
+  {} as ICartContextData
+);
+
+export const CartDataContext = ({
+  children,
+}: ICartDataContextProps): JSX.Element => {
+  const [cart, setCart] = useState<IProduct[]>(() => {
     const cartStorage = localStorage.getItem("MarketApp:cart");
 
     if (cartStorage) return JSON.parse(cartStorage);
@@ -40,23 +59,33 @@ export function useCart() {
     return [];
   });
 
-  function productAmount(product: IProduct | ICart) {
+  const [updatedCart, setUpdatedCart] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    if (updatedCart.length > 0) {
+      setCart(updatedCart);
+    }
+  }, [updatedCart]);
+
+  function productAmount(product: IProduct | IProductInCart) {
     const productInCart = cart.find((prod) => prod.id === product.id);
 
     return productInCart ? Number(productInCart.amount) : 0;
   }
 
-  function addProduct(product: IProduct) {
+  function addProduct(product: IProductInCart) {
     const productInCart = cart.find((prod) => prod.id === product.id);
 
     if (!productInCart) {
       if (product.stock > 0) {
-        setCart([...cart, { ...product, amount: 1 }]);
+        setUpdatedCart([...cart, { ...product, amount: 1 }]);
+
         localStorage.setItem(
           "MarketApp:cart",
           JSON.stringify([...cart, { ...product, amount: 1 }])
         );
 
+        toast.success("Adicionado ao Carrinho");
         return;
       }
     }
@@ -66,17 +95,19 @@ export function useCart() {
         p.id === product.id ? { ...p, amount: Number(p.amount) + 1 } : p
       );
 
-      setCart(updatedCart);
-      console.log(updatedCart);
+      setUpdatedCart([...updatedCart]);
 
       localStorage.setItem("MarketApp:cart", JSON.stringify(updatedCart));
+
+      toast.success("Adicionado mais uma unidade");
       return;
     } else {
-      alert("Produto sem estoque!");
+      toast.warning("Produto sem Estoque");
     }
+    return;
   }
 
-  function decrementProduct(product: IProduct | ICart) {
+  function decrementProduct(product: IProduct | IProductInCart) {
     const productInCart = cart.find((prod) => prod.id === product.id);
 
     if (productInCart) {
@@ -85,29 +116,45 @@ export function useCart() {
           p.id === product.id ? { ...p, amount: Number(p.amount) - 1 } : p
         );
 
-        setCart(updatedCart);
-        console.log(updatedCart);
+        setUpdatedCart([...updatedCart]);
 
         localStorage.setItem("MarketApp:cart", JSON.stringify(updatedCart));
+
+        toast.info("Removido uma unidade");
         return;
       } else {
         deleteProduct(product);
+        toast.warn("Produto removido do carrinho");
       }
     }
   }
 
-  function deleteProduct(product: IProduct | ICart) {
+  function deleteProduct(product: IProduct | IProductInCart) {
     const productInCart = cart.find((prod) => prod.id === product.id);
 
     if (productInCart) {
       const updatedCart = cart.filter((p) => p.id !== productInCart.id);
 
-      setCart(updatedCart);
+      setUpdatedCart([...updatedCart]);
 
       localStorage.setItem("MarketApp:cart", JSON.stringify(updatedCart));
+
+      toast.warn("Produto removido do carrinho");
       return;
     }
   }
 
-  return { cart, productAmount, addProduct, decrementProduct, deleteProduct };
-}
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        productAmount,
+        addProduct,
+        decrementProduct,
+        deleteProduct,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
